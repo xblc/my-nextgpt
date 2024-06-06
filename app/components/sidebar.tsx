decc6a3ce915412c8c944639fe38b0adb3e0e22f
xban <x_ban@qq.com>
2024-06-06 22:34:10 +0800

    1

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          防失联请加 792494720
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
